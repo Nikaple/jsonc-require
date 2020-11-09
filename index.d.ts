316be5c3711dd8f14f4587a7dd9b989fcc2bfe5d
nikaple <nikaple1@gmail.com>
2020-11-09 16:35:53 +0800

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,1 @@
-declare module '*.json' {
-  type JsonValue = string | number | boolean | null
-  type JsonArray = Array<object | JsonValue | any[]>
-  type JsonObject = Record<string, object | JsonArray | JsonValue>
-
-  const json: Record<string, JsonObject | JsonArray>
-}
-
-export = {}
+export {}
